219a21ec0ea5785cbd69a0cca174f6647adc9d9e
KonstantinShiriton <konstantinshiriton@coherentsolutions.com>
2024-10-14 03:20:58 -0700

    Added app to test connection to Azure SQL

---
+++ AzureSqlTest/Program.cs
@@ -0,0 +1,34 @@
+﻿#pragma warning disable IDE0063
+
+using System.Data.SqlClient;
+
+namespace AzureSqlTest
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // Set Inbound IP In Firewall Settings
+            // SQL DB Server -> Settings -> Connection Strings
+            string connectionString = "";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    Console.WriteLine("Connection successful!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
